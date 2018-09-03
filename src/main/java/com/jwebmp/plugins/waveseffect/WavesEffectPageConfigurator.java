f7c62d49834fb84d6f91cd4165f9f916d14a71be
GedMarc <ged_marc@hotmail.com>
2018-09-03 22:50:45 +0200

    Make page configuration optional, update the app

--- src/main/java/com/jwebmp/plugins/waveseffect/WavesEffectPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/waveseffect/WavesEffectPageConfigurator.java
@@ -14,8 +14,36 @@ public class WavesEffectPageConfigurator
 	private static final JavascriptReference reference = new JavascriptReference("WavesEffectJSReference", 0.76, "bower_components/waves/dist/waves.min.js");
 	private static final CSSReference cssReference = new CSSReference("WavesEffectCSSSReference", 0.76, "bower_components/waves/dist/waves.min.css");
 	private static final CSSReference cssOverridesReference = new CSSReference("WavesEffectCSSSOverideReference", 0.76, "waves_overrides/waves_colours.min.css");
-
 	private static final Feature initFeature = new WavesAttachFeature(null);
+	/**
+	 * If this configurator is enabled
+	 */
+	private static boolean enabled = true;
+
+	/**
+	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static boolean isEnabled()
+	{
+		return WavesEffectPageConfigurator.enabled;
+	}
+
+	/**
+	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @param mustEnable
+	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static void setEnabled(boolean mustEnable)
+	{
+		WavesEffectPageConfigurator.enabled = mustEnable;
+	}
 
 	/**
 	 * Returns the current script reference
@@ -49,5 +77,11 @@ public class WavesEffectPageConfigurator
 			page.addFeature(WavesEffectPageConfigurator.initFeature);
 		}
 		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return WavesEffectPageConfigurator.enabled;
 	}
 }
