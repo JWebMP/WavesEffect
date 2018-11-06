6f2c2fe1ee45ecc4498878c89e4e310bccb1d085
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:28:59 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/waveseffect/implementations/WavesEffectsExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.waveseffect.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WavesEffectsExclusionsModule
+		implements IGuiceScanModuleExclusions<WavesEffectsExclusionsModule>,
+				           IGuiceScanJarExclusions<WavesEffectsExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-waveseffect-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.waveseffect");
+		return strings;
+	}
+}
