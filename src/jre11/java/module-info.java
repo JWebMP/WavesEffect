92f9a5966121e5cf39193f6018e4a43f89265ce5
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:56 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.waveseffect.WavesEffectPageConfigurator;
-import com.jwebmp.plugins.waveseffect.implementations.WavesEffectsExclusionsModule;
-
 module com.jwebmp.plugins.waveseffect {
 	exports com.jwebmp.plugins.waveseffect;
 
@@ -11,10 +5,10 @@ module com.jwebmp.plugins.waveseffect {
 	requires java.validation;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with WavesEffectPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.waveseffect.WavesEffectPageConfigurator;
 
-	provides IGuiceScanJarExclusions with WavesEffectsExclusionsModule;
-	provides IGuiceScanModuleExclusions with WavesEffectsExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.waveseffect.implementations.WavesEffectsExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.waveseffect.implementations.WavesEffectsExclusionsModule;
 
 	//opens com.jwebmp.plugins.waveseffect to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
