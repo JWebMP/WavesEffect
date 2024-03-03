1741c8cf54b48508119fd1e9bf648899aca4a83a
GedMarc <marc.magon@gmail.com>
2024-03-04 01:57:25 +0200

    Add Waves Effect

--- src/main/java/com/jwebmp/plugins/waveseffect/WavesEffectPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/waveseffect/WavesEffectPageConfigurator.java
@@ -6,45 +6,46 @@ import com.jwebmp.core.base.angular.client.annotations.angularconfig.*;
 import com.jwebmp.core.base.angular.client.annotations.typescript.*;
 import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.plugins.PluginStatus;
+import com.jwebmp.core.services.IPage;
 import com.jwebmp.core.services.IPageConfigurator;
 
 import jakarta.validation.constraints.NotNull;
 
 @PluginInformation(pluginName = "Waves Effects ",
-		pluginUniqueName = "waves",
-		pluginDescription = "Waves Effects",
-		pluginVersion = "1.0",
-		pluginDependancyUniqueIDs = "jquery",
-		pluginCategories = "effects",
-		pluginSubtitle = "the https://json.geoiplookup.io/ ip locator service ",
-		pluginGitUrl = "https://github.com/GedMarc/JWebMP-WavesEffect",
-		pluginSourceUrl = "https://mdbootstrap.com/docs/standard/methods/ripple/",
-		pluginWikiUrl = "https://github.com/GedMarc/JWebMP-WavesEffect/wiki",
-		pluginOriginalHomepage = "https://mdbootstrap.com/docs/standard/methods/ripple/",
-		pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.effects/jwebmp-waveseffect",
-		pluginIconUrl = "",
-		pluginIconImageUrl = "",
-		pluginLastUpdatedDate = "2020/12/14",
-		pluginGroupId = "com.jwebmp.plugins.effects",
-		pluginArtifactId = "jwebmp-waveseffect",
-		pluginModuleName = "com.jwebmp.plugins.waveseffect",
-		pluginStatus = PluginStatus.Released
+        pluginUniqueName = "waves",
+        pluginDescription = "Waves Effects",
+        pluginVersion = "1.0",
+        pluginDependancyUniqueIDs = "jquery",
+        pluginCategories = "effects",
+        pluginSubtitle = "the https://json.geoiplookup.io/ ip locator service ",
+        pluginGitUrl = "https://github.com/GedMarc/JWebMP-WavesEffect",
+        pluginSourceUrl = "https://mdbootstrap.com/docs/standard/methods/ripple/",
+        pluginWikiUrl = "https://github.com/GedMarc/JWebMP-WavesEffect/wiki",
+        pluginOriginalHomepage = "https://mdbootstrap.com/docs/standard/methods/ripple/",
+        pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.effects/jwebmp-waveseffect",
+        pluginIconUrl = "",
+        pluginIconImageUrl = "",
+        pluginLastUpdatedDate = "2020/12/14",
+        pluginGroupId = "com.jwebmp.plugins.effects",
+        pluginArtifactId = "jwebmp-waveseffect",
+        pluginModuleName = "com.jwebmp.plugins.waveseffect",
+        pluginStatus = PluginStatus.Released
 )
 @TsDependency(value = "node-waves", version = "*")
 @NgScript("node_modules/node-waves/src/js/waves.js")
 @NgStyleSheet("node_modules/node-waves/src/scss/waves.scss")
 public class WavesEffectPageConfigurator
-		implements IPageConfigurator<WavesEffectPageConfigurator>
+        implements IPageConfigurator<WavesEffectPageConfigurator>
 {
-	@Override
-	public @NotNull Page<?> configure(Page<?> page)
-	{
-		return page;
-	}
+    @Override
+    public @NotNull IPage<?> configure(IPage<?> page)
+    {
+        return page;
+    }
 
-	@Override
-	public boolean enabled()
-	{
-		return true;
-	}
+    @Override
+    public boolean enabled()
+    {
+        return true;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,13 @@
 import com.jwebmp.plugins.waveseffect.implementations.WavesEffectsInclusionModule;
 
 module com.jwebmp.plugins.waveseffect {
-	exports com.jwebmp.plugins.waveseffect;
+    exports com.jwebmp.plugins.waveseffect;
 
-	requires transitive com.jwebmp.core.angular;
-	requires com.guicedee.jsonrepresentation;
-	
-	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.waveseffect.WavesEffectPageConfigurator;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with WavesEffectsInclusionModule;
+    requires com.jwebmp.client;
+    requires com.jwebmp.core;
+    requires transitive com.jwebmp.core.base.angular.client;
+    requires com.guicedee.jsonrepresentation;
+
+    provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.waveseffect.WavesEffectPageConfigurator;
+    provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with WavesEffectsInclusionModule;
 }
